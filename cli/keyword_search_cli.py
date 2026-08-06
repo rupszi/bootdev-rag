2cdc61845ef3ef53c11e206d7c32b62211610535
Pal Megyes <pal.megyes@hotmail.com>
2026-08-06 10:39:22 +0200

    implemented stopword discard functionality to find meaniingful results only

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -14,18 +14,22 @@ def main() -> None:
 
     with open("data/movies.json") as f:
         movies = json.load(f)
-    
+
+    with open("data/stopwords.txt") as f:
+        stopwords = set(f.read().translate(str.maketrans('', '', string.punctuation)).lower().splitlines())
+        
+
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
             result = []
             punc_table = str.maketrans("", "", string.punctuation)
             clean_text = args.query.translate(punc_table).lower()
-            tokenized_query = clean_text.split()
+            tokenized_query = [word for word in clean_text.split() if word not in stopwords]
 
             for movie in movies["movies"]:
                 clean_title = movie["title"].translate(punc_table).lower()
-                tokenized_title = clean_title.split()
+                tokenized_title = [word for word in clean_title.split() if word not in stopwords]
                 if any(q_token in t_token for q_token in tokenized_query for t_token in tokenized_title):
                     result.append(movie)
 
