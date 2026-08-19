4c09c01e3d6f8c61de8ded1c95657a3f7eb79286
Pal Megyes <pal.megyes@hotmail.com>
2026-08-19 10:14:49 +0200

    added to the semantic cli the import of the verify_model class and created subcommands

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,14 +1,24 @@
 import argparse
+from cli.lib.semantic_search import verify_model
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Register 'verify' subcommand
+    subparsers.add_parser("verify", help="Verify model loading and sequence length")
+
     args = parser.parse_args()
 
     match args.command:
+        case "verify":
+            verify_model()
         case _:
             parser.print_help()
 
 
 if __name__ == "__main__":
     main()
+
+    
